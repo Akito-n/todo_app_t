a0c042a61444123bed896d5a9b57a3b731155800
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-02-13 17:42:46 +0900

    test

--- spec/features/new_task_interface_spec.rb
+++ spec/features/new_task_interface_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'task_CRUD', type: :feature do
   let(:task) {create(:task)}
-
-  let(:task1) {create(:order_task, :create2013_01_01)}
+  let(:task1) {create(:order_task, :created2013_01_01)}
   let(:task2) {create(:order_task, :create2012_01_01)}
   let(:task3) {create(:order_task, :create2011_01_01)}
 
@@ -13,9 +12,8 @@ RSpec.feature 'task_CRUD', type: :feature do
       click_link '新規作成'
       fill_in 'task_title', with: 'new title'
       fill_in 'task_description', with: 'new description'
-      click_button '登録する'
+      click_button '登録'
       expect(page).to have_css '.alert'
-      expect(page).to have_content 'new title'
     }.to change { Task.count }.by(1)
 
   end
@@ -24,7 +22,7 @@ RSpec.feature 'task_CRUD', type: :feature do
     visit edit_task_path(task)
     fill_in 'task_title', with: 'title_edited'
     fill_in 'task_description', with: 'description_edited'
-    click_button '更新する'
+    click_button '更新'
     expect(page).to have_css '.alert'
     expect(page).to have_content 'title_edited'
   end
@@ -40,12 +38,18 @@ RSpec.feature 'task_CRUD', type: :feature do
 
 
   scenario 'order by created_at DESC' do
-    visit root_path
     task2
     task1
     task3
-    taskarray = [task1, task2, task3]
-    expect(Task.all.order('created_at DESC')).to eq taskarray
+    task_array = [task1, task2, task3]
+    expect(Task.order('created_at DESC')).to eq task_array
+  end
+
+  scenario 'validation if title is empty' do
+    visit root_path
+    fill_in 'task_description', with: 'new description'
+    click_button '登録'
+    expect(page).to have_content 'titleを入力してください'
   end
 
 end
