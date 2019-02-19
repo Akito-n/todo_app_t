d5bbf11b4de08514b59c21617c498888ae4cd9ed
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-02-19 10:58:20 +0900

    add coulum status to Task and enum create status

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,9 @@ class Task < ApplicationRecord
   validates :description, length: { maximum: 200 }
 
 
+  enum status: {waiting: 0, warking: 1, completed: 2}
+
+
   def self.sort(q='created_at DESC')
     if q == 'asc'
       q = 'term ASC'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_16_074942) do
+ActiveRecord::Schema.define(version: 2019_02_19_013537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_074942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "term"
+    t.integer "status", default: 0, null: false
   end
 
 end
