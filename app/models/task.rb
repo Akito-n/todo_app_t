44d353d6b8b204ba07ae9bcf8df1cb0edcc257c7
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-02-20 15:02:46 +0900

    fix scope sort_term

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,15 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 200 }
 
-  scope :sort_term, -> (p) { order(p) }
+  scope :sort_term, -> (p) {
+  case p
+  when 'created_at'
+    order(created_at: :DESC)
+  when 'asc'
+    order(term: :asc)
+  when 'desc'
+    order(term: :desc)
+  end
+  }
 
 end
