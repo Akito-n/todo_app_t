41e0cbffcdf203402f6fcf79597a4dea56fb5c09
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-02-20 11:09:19 +0900

    メソッド名がかぶっていた問題の修正とActiveRecord::Reration用にscopeに書き換え

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
 
   def index
-    @tasks = Task.sort(params[:sort])
+    @tasks = Task.sort_term(params[:sort])
   end
 
   def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,6 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 200 }
 
-
-  def self.sort(q='created_at DESC')
-    if q == 'asc'
-      q = 'term ASC'
-    elsif q == 'desc'
-      q = 'term DESC'
-    end
-    order(q)
-  end
+  scope :sort_term, -> (p) { order(p) }
 
 end
