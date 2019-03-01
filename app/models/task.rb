eda7911b3b9a283af94a604e9dad25d3d65ade2b
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-01 12:09:53 +0900

    タスクに紐づくタグの設置処理をtaskモデルに記載

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,4 +14,21 @@ class Task < ApplicationRecord
 
   enum priority: { low: 0, middle: 1, high: 2 }
 
+
+  def save_tags(tags)
+    current_tags = self.tags.pluck(:body) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+
+    old_tags.each do |old_name|
+      self.tags.delete(Tag.find_by(name: old_name))
+    end
+
+    new_tags.each do |new_name|
+      task_tag = Tag.find_or_create_by(name: new_name)
+      self.tags << task_tag
+    end
+  end
+
 end
