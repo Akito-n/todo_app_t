d97d1d1953bf4965320767a292253b3ae95de968
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-01 10:33:19 +0900

    関連モデルの順番を正しい形に変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :user
+  has_many :task_tags
+  has_many :tags, through: :task_tags
+
+
+  accepts_nested_attributes_for :task_tags
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 200 }
