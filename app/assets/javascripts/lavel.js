5375db0e64586b48301ee03cb1cc2eb6942eccec
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-03 22:26:12 +0900

    tag-itのformをつける

---
+++ app/assets/javascripts/lavel.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function() {
+  $("#task_lavels").tagit({
+    fieldName:"tags[]"
+  });
+  if (gon.lavel_list) {
+    tags = gon.lavel_list.split(",");
+    for (i in tags){
+       $('#task_lavels').tagit('createTag', tags[i])
+     }
+  }
+})
+console.log('s');
