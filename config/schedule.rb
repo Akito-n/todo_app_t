5cc7b7f912d569cdc39fbe617e1e3002827b98b2
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-06 12:46:09 +0900

    wheneverを使ったスケジュール構築crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,12 +18,13 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-
-require File.expand_path(File.dirname(__FILE__) + '/enviroment')
+require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 
 set :output, "#{Rails.root}/log/cron.log"
 
 #clear cache
-every
+every 1.day, at: '12:00 am' do
+  runner "Notification.set_notification"
+end
