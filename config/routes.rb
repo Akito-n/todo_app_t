8de99e8ae1fbba6d2b4c208608a7e9ed5a3d312d
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-10 09:14:57 +0900

    group routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/new'
-  get 'groups/create'
-  get 'groups/edit'
-  get 'groups/update'
-  get 'groups/destroy'
   ActiveAdmin.routes(self)
   root to: 'tasks#index'
   get 'signup', to: 'users#new'
   resources :tasks
   resources :users, only: [:new, :create]
+  resources :groups
   get 'users', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
