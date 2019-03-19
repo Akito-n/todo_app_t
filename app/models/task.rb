70b650b33a948aea64b1e95ca497c36cbc07601b
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-19 19:24:36 +0900

    タスクモデルにinclude Ranked_model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+  include RankedModel
+  ranks :row_order, class_name: 'Task'
+
   belongs_to :user
   belongs_to :group
   has_one :notification, dependent: :destroy
