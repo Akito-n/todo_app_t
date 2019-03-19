ca1110218cd53b1d921bcf65d2f8515b27016387
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-19 18:45:57 +0900

    error

--- db/migrate/20190206092548_create_tasks.rb
+++ db/migrate/20190206092548_create_tasks.rb
@@ -2,7 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.text :description 
+      t.text :description
+      t.integer :row_order
       t.timestamps
     end
   end
