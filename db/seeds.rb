22261aab6ea21ffeb901cd7c7e871f6ac3b7b906
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-20 11:23:58 +0900

    seed 追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,8 +29,32 @@
  end
 
  User.create!(
+   id: 11,
    name: 'Admin',
    email: 'sample@test.com',
    password: 'password',
    role: 1
+ )
+
+ Group.create!(
+   id: 1,
+   name: 'テストグループ',
+   user_id: 11
+ )
+
+ 10.times do |t|
+   title = Faker::Games::Pokemon.name
+   description = Faker::Book.title
+   Task.create!(
+     title: title,
+     description: description,
+     term: Faker::Time.between(10.years.ago, 0.years.ago, :all).to_s[0, 10],
+     user_id: 11,
+     group_id: 1
+    )
+ end
+
+ Member.create!(
+   user_id: 11,
+   group_id: 1
  )
