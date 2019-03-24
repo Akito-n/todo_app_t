fdf81c3ba2d65ff2cf979e6756848a9152f7cfe0
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-24 19:53:12 +0900

    add 環境変数

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,14 +94,14 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method       = :smtp
-  config.action_mailer.default_url_options   = { host: 'nagai-task-app.herokuapp.com' }
+  config.action_mailer.default_url_options   = { host: ENV['SENDGRID_HOST'] }
   ActionMailer::Base.smtp_settings           = {
-      address:              'smtp.sendgrid.net',
-      port:                 '587',
+      address:              ENV['SENDGRID_ADDRESS'],
+      port:                 ENV['SENDGRID_PORT'],
       authentication:       :plain,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
-      domain:               'heroku.com',
+      domain:               ENV['SENDGRID_DOMAIN'],
       enable_starttls_auto: true
   }
 end
