0a26b4868a5f60e88106bb7c61e369c8cf30cb61
Akito Nagai <a.nagai@mof-mof.co.jp>
2019-03-24 19:16:47 +0900

    rmagick -> mini_magick

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-   include CarrierWave::RMagick
+   include CarrierWave::MiniMagick
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
